d49daee80d7ddec518c8545da1df5b4e499fa281
Becky Torrey <torrey1028@gmail.com>
2022-09-09 14:59:12 -0700

    Add CMake support

---
+++ test_cmake/toolchain.cmake
@@ -0,0 +1,16 @@
+set(CMAKE_SYSTEM_NAME Generic)
+set(CMAKE_SYSTEM_PROCESSOR arm)
+
+set(CMAKE_C_COMPILER "arm-none-eabi-gcc.exe")
+set(CMAKE_CXX_COMPILER "arm-none-eabi-g++.exe")
+
+set(CMAKE_EXE_LINKER_FLAGS "--specs=nosys.specs -mcpu=cortex-m4 -mthumb -mfpu=fpv4-sp-d16 -mfloat-abi=hard -specs=nano.specs -lc -lm -lnosys -Wl,--gc-sections" CACHE INTERNAL "")
+set(CMAKE_C_FLAGS "-mcpu=cortex-m4 -mthumb -mfpu=fpv4-sp-d16 -mfloat-abi=hard -Og -Wall -fdata-sections -ffunction-sections -g -gdwarf-2" CACHE INTERNAL "")
+set(CMAKE_CXX_FLAGS "-mcpu=cortex-m4 -mthumb -mfpu=fpv4-sp-d16 -mfloat-abi=hard -Og -Wall -fdata-sections -ffunction-sections -g -gdwarf-2" CACHE INTERNAL "")
+
+set(CMAKE_CROSSCOMPILING ON CACHE BOOL "Cross-compiling" FORCE)
+
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
