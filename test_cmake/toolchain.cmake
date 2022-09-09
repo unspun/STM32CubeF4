c43fed4deca3d92178cb1cfc95b56ed80a8ed282
Becky Torrey <torrey1028@gmail.com>
2022-09-09 15:29:26 -0700

    Change toolchain

--- test_cmake/toolchain.cmake
+++ test_cmake/toolchain.cmake
@@ -1,8 +1,8 @@
 set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_SYSTEM_PROCESSOR arm)
 
-set(CMAKE_C_COMPILER "arm-none-eabi-gcc.exe")
-set(CMAKE_CXX_COMPILER "arm-none-eabi-g++.exe")
+set(CMAKE_C_COMPILER "arm-none-eabi-gcc")
+set(CMAKE_CXX_COMPILER "arm-none-eabi-g++")
 
 set(CMAKE_EXE_LINKER_FLAGS "--specs=nosys.specs -mcpu=cortex-m4 -mthumb -mfpu=fpv4-sp-d16 -mfloat-abi=hard -specs=nano.specs -lc -lm -lnosys -Wl,--gc-sections" CACHE INTERNAL "")
 set(CMAKE_C_FLAGS "-mcpu=cortex-m4 -mthumb -mfpu=fpv4-sp-d16 -mfloat-abi=hard -Og -Wall -fdata-sections -ffunction-sections -g -gdwarf-2" CACHE INTERNAL "")
